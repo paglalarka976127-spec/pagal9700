664432d53965403dd40263013f81df1eb996c81e
paglalarka976127-spec <paglalarka976127@gmail.com>
2026-04-09 14:09:02 +0530

    Create nach.js

---
+++ Priyansh/commands/nach.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+module.exports.config = {
+  name: "nach",
+    version: "2.1.1",
+  hasPermssion: 0,
+  credits: "𝐏𝐑𝐈𝐘𝐀𝐍𝐒𝐇𝐈 𝐊𝐀𝐔𝐑", 
+  description: "Just Respond",
+  commandCategory: "no prefix",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = async ({ api, event, Users, Currencies, args, utils, client, global }) => {
+  var name = await Users.getNameUser(event.senderID);
+  var { threadID, messageID } = event;
+  let react = event.body.toLowerCase();
+  if(react.includes("nach") ||
+react.includes("Nachna")) {
+    var msg = {
+        body: `Basanti ne nachna chor diya hai ab😒😒😒`,attachment: fs.createReadStream(__dirname + `/noprefix/nach.mp4`)
+      }
+      api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("😆", event.messageID, (err) => {}, true)
+    }
+  }
+  module.exports.run = async ({ api, event, Currencies, args, utils, client, global }) => {
+
+  }
